4da81d274f530e3877f24f19330a6221be49c47c
Suman Rajput <107524156+sumansingh05@users.noreply.github.com>
2024-06-28 13:47:22 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,16 @@
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
+setInterval(() => {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    hrotation = 30*h + m/2;
+    mrotation = 6*m;
+    srotation = 6*s;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+},1000);
